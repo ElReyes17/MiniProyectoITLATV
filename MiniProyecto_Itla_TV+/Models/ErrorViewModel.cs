56641365ee4a85b45548e1b89b271c82ff3053a9
Eliott Reyes <eliottreyes22@gmail.com>
2023-09-16 23:57:39 -0400

    Agregar archivos de proyecto.

---
+++ MiniProyecto_Itla_TV+/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniProyecto_Itla_TV_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
