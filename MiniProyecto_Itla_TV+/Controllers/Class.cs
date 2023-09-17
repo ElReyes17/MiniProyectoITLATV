46f28854958cb6cf9d448d35f2c7f0f576c0c110
Eliott Reyes <eliottreyes22@gmail.com>
2023-09-16 23:59:07 -0400

    COMIT INICIAL CON LA CREACIÓN DEL PROYECTO

---
+++ MiniProyecto_Itla_TV+/Controllers/Class.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniProyecto_Itla_TV_.Controllers
+{
+    public class Class : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
