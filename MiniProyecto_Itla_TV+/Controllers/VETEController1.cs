85851d0c3e4a293024ff6bec6dce294b46348316
Eliott Reyes <eliottreyes22@gmail.com>
2023-09-17 00:01:17 -0400

    VETE....

---
+++ MiniProyecto_Itla_TV+/Controllers/VETEController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniProyecto_Itla_TV_.Controllers
+{
+    public class VETEController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
