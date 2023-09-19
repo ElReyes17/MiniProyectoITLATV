f44ff76efb279e86e922a704f1ea071e4c23491a
Eliott Reyes <eliottreyes22@gmail.com>
2023-09-19 00:20:19 -0400

    Se crearon las relaciones de las tablas modelos

--- DataBase/Modelos/Productoras.cs
+++ DataBase/Modelos/Productoras.cs
@@ -11,9 +11,9 @@ namespace DataBase.Modelos
 
         public int Id { get; set; } 
         public string Nombre { get; set; } = null!;
-
-
-
+      
+        
+        
         public ICollection<Series> Series { get; set; } = null!;
 
        
--- DataBase/TVContext.cs
+++ DataBase/TVContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataBase.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,17 @@ using System.Threading.Tasks;
 
 namespace DataBase
 {
-   public class TVContext
+   public class TVContext : DbContext
     {
+        public TVContext(DbContextOptions<TVContext> opciones) : base(opciones){ }
 
+        public DbSet<Series> Series { set; get; }
+
+        public DbSet<Generos> Generos { set; get; }
+
+        public DbSet<SeriesGeneros> SeriesGeneros { set; get; }
+
+        public DbSet<Productoras> Productoras { set; get; }
 
     }
 }
